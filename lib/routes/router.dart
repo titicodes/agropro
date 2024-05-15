dac68297dfa2facdc0e8e93c4bfb057097149117
titicodes <anietimfoneeffiong@gmail.com>
2024-05-15 17:26:05 +0100

    diagnosis

---
+++ lib/routes/router.dart
@@ -0,0 +1,37 @@
+import 'package:agropro/pages/base/search/camera.dart';
+import 'package:agropro/pages/base/search/plant_identification';
+import 'package:agropro/pages/base/search/search.dart';
+import 'package:agropro/pages/base/search/search_results.dart';
+import 'package:agropro/pages/home/diagnose_view.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/main_nav.dart';
+import 'routes.dart';
+
+class Routers {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case homeRoute:
+        return MaterialPageRoute(builder: (_) => MainNva());
+      case seearchRoute:
+        return MaterialPageRoute(builder: (_) => Search());
+      case searchResultRoute:
+        return MaterialPageRoute(builder: (_) => SearchResults());
+      case diagnosisRoute:
+        return MaterialPageRoute(builder: (_) => const DiseaseDiagnosView());
+      case cameraRoute:
+        return MaterialPageRoute(builder: (_) => Camera());
+      case plantIdentityRoute:
+        return MaterialPageRoute(builder: (_) => PlantIdentification());
+
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(
+              child: Text('No route defined for ${settings.name}'),
+            ),
+          ),
+        );
+    }
+  }
+}
